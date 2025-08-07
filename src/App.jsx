57c359905b9ea51c87ce98007e0da1e7f1955588
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2025-08-07 23:17:25 +0530

    benefit sec done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSec from './section/HeroSec'
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import gsap from 'gsap';
 import MessageSec from './section/MessageSec';
 import FlavourSec from './section/FlavourSec';
+import { useGSAP } from '@gsap/react';
+import NutritionSec from './section/NutritionSec';
+import BenefitSec from './section/BenefitSec';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true
+    })
+  })
+
   return (
     <main>
       <NavBar />
-      <HeroSec />
-      <MessageSec />
-      <FlavourSec />
-      <div className='h-dvd'></div>
+      <div id="smooth-wrapper">
+        <div id='smooth-content'>
+          <HeroSec />
+          <MessageSec />
+          <FlavourSec />
+          <NutritionSec />
+          <BenefitSec />
+          <div className='h-dvh'></div>
+        </div>
+      </div>
     </main>
   )
 }
